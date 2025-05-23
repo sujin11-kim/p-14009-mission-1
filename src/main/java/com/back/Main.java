bb779717ebd31170818194e4bf83f8abf23696b1
sujin11-kim <su091876@gmail.com>
2025-05-23 10:19:25 +0900

    step4

--- src/main/java/com/back/Main.java
+++ src/main/java/com/back/Main.java
@@ -8,6 +8,8 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
+        int lastId = 0;
+
         while (true) {
             System.out.print("명령) ");
             String cmd = scanner.nextLine().trim();
@@ -20,7 +22,8 @@ public class Main {
                 System.out.print("작가 : ");
                 String wiseSayingAuthor = scanner.nextLine().trim();
 
-                System.out.println("1번 명언이 등록되었습니다.");
+
+                System.out.printf("%d번 명언이 등록되었습니다.\n", ++lastId);
             }
         }
 
