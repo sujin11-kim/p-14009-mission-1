2c4847a3b7e43a378f9b759f492f4e9fadb16c15
sujin11-kim <su091876@gmail.com>
2025-05-23 10:09:14 +0900

    step1

---
+++ src/main/java/com/back/Main.java
@@ -0,0 +1,22 @@
+package com.back;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("== 명언 앱 ==");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("명령) ");
+            String cmd = scanner.nextLine().trim();
+
+            if (cmd.equals("종료")) {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+}
